0f653eb2614d0255e2067eadc785b7fd87a41073
itzdoxa <153353785+itzdoxa@users.noreply.github.com>
2024-04-29 14:42:50 +0530

    added various color modes

--- script.js
+++ script.js
@@ -1,58 +1,80 @@
 const createBtn = document.querySelector(".create");
-var container = document.createElement("div");
+const container = document.createElement("div");
+const eraser = document.querySelector(".eraser")
+const random = document.querySelector(".random-color");
+const colorPicker = document.querySelector("#color-picker");
+const black = document.querySelector(".black");
+
 container.classList.add("container");
 document.body.appendChild(container);
 
 let num;
 createBtn.addEventListener('click', function(){
-     num = prompt("number of squares per side");
-         if(num > 100) {
-             alert("input should be less than 100");
-             return;
-         }      
-         else {
-             removeGrid();
-             createGrid();
-         }
+   num = prompt("number of squares per side");
+      if(num > 100) {
+        return alert("input should be less than 100");
+       }      
+      else {
+          removeGrid();
+          createGrid();
+       }
 });
 
-let currentDiv;
-container.addEventListener('mouseover', function(event) {
-   if(container.firstChild) {
-       currentDiv = event.target;
-      // fix: once a div has gotten a color, dont let it change 
-      // set a default color 
-      // let user select a color
-      colorThis(currentDiv); 
-  }
-});
-
+let div;
 function createGrid(){
-   let divSize = 960/num; 
-     for(let i = 0; i < num * num; i++) {
-        let div = document.createElement("div");
-        div.style.height = divSize + "px";
-        div.style.width = divSize + "px";
-        div.style.outline = "0.1px solid black";
-        div.classList.add("box");
-        container.appendChild(div);
-     };   
+  let divSize = 780/num; 
+    for(let i = 0; i < num * num; i++) {
+       div = document.createElement("div");
+       div.style.height = divSize + "px";
+       div.style.width = divSize + "px";
+       container.appendChild(div);
+    };     
 };
 
 function removeGrid() {
-  while (container.firstChild) {
-      container.removeChild(container.firstChild);
-  }
+ while (container.firstChild) {
+     container.removeChild(container.firstChild);
+ }
 };
 
-function randomColor(){ 
-       let red = Math.ceil(Math.random()*255);
-       let green = Math.ceil(Math.random()*255);
-       let blue = Math.ceil(Math.random()*255);
-       return rgbValue = `rgb(${red}, ${green}, ${blue})`;
+let currentDiv;
+let rgbValue;
+let colorMode = "default";
+container.addEventListener('mouseover', function(event) {
+   if(container.firstChild) {
+      currentDiv = event.target;
+          if(colorMode == "default") {
+               currentDiv.style.background = "black";
+          }
+          else if(colorMode == "random"){
+               currentDiv.style.background = randomColor();
+          }
+          else if(colorMode == "erase"){
+               currentDiv.style.background = "";
+          }
+          else if(colorMode == "colorPicker"){
+               currentDiv.style.background = colorPicker.value;
+          };
+    };      
+ });
+
+function randomColor(){
+   let red = Math.ceil(Math.random()*255);
+   let green = Math.ceil(Math.random()*255);
+   let blue = Math.ceil(Math.random()*255);
+   return rgbValue = `rgb(${red}, ${green}, ${blue})`;
 };
 
-function colorThis(element){
-   element.style.background = randomColor();
- };
-
+eraser.addEventListener('click', function(){
+        colorMode = "erase";
+ });
+random.addEventListener('click', function(){
+        colorMode = "random";
+ });
+black.addEventListener('click', function(){
+        colorMode = "default";
+});
+colorPicker.addEventListener('input', function(){
+        colorMode = "colorPicker";
+})
+    
